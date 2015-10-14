b1c8efaf55e38d2339f60f55e669c6ce9af7c6fb
andypay <andypay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-14 13:27:15 +0000

    Added new payment methods * Billink * Givacard * Wijncadeau
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-paynl-payment-methods/tags/2.3.12@1265861 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/classes/Pay/Gateways.php
@@ -0,0 +1,226 @@
+<?php
+
+class Pay_Gateways
+{
+
+    const STATUS_PENDING = 'PENDING';
+    const STATUS_CANCELED = 'CANCELED';
+    const STATUS_SUCCESS = 'SUCCESS';
+
+    public static function _getGateways($arrDefault)
+    {
+
+        $paymentOptions = array(
+            'Pay_Gateway_Afterpay',
+			'Pay_Gateway_Billink',
+            'Pay_Gateway_Cartebleue',
+            'Pay_Gateway_Clickandbuy',
+            'Pay_Gateway_Fashioncheque',
+            'Pay_Gateway_Gezondheidsbon',
+            'Pay_Gateway_Giropay',
+			'Pay_Gateway_Givacard',
+            'Pay_Gateway_Ideal',
+            'Pay_Gateway_Incasso',
+            'Pay_Gateway_Maestro',
+            'Pay_Gateway_Minitixsms',
+            'Pay_Gateway_Mistercash',
+            'Pay_Gateway_Mybank',
+            'Pay_Gateway_Overboeking',
+            'Pay_Gateway_Paypal',
+            'Pay_Gateway_Paysafecard',
+            'Pay_Gateway_Phone',
+            'Pay_Gateway_Podiumcadeaukaart',
+            'Pay_Gateway_Postepay',
+            'Pay_Gateway_Sofortbanking',
+            'Pay_Gateway_Visamastercard',
+			'Pay_Gateway_Wijncadeau',
+            'Pay_Gateway_Yourgift',
+        );
+
+        $arrDefault = array_merge($arrDefault, $paymentOptions);
+
+        return $arrDefault;
+    }
+
+    public static function _addGlobalSettings($settings)
+    {
+
+        $loadedPaymentMethods = "";
+        try
+        {
+            Pay_Helper_Data::loadPaymentMethods();
+
+            $arrOptions = Pay_Helper_Data::getOptions();
+            $loadedPaymentMethods .= '<br /><br />' . __('The following payment methods can be enabled', 'woocommerce-payment-paynl');
+
+            $loadedPaymentMethods .= '<ul style="width:900px;">';
+            foreach ($arrOptions as $option)
+            {
+                $loadedPaymentMethods .= '<li style="float: left; width:300px;"><img src="' . $option['image'] . '" alt="' . $option['name'] . '" title="' . $option['name'] . '" /> ' . $option['name'] . '</li>';
+            }
+            $loadedPaymentMethods .= '</ul>';
+            $loadedPaymentMethods .= '<div class="clear"></div>';
+        } catch (Pay_Exception $e)
+        {
+            $loadedPaymentMethods = '<span style="color:red; font-weight:bold;">Error: ' . $e->getMessage() . '</span>';
+        }
+
+
+        $updatedSettings = array();
+
+        $addedSettings = array();
+        $addedSettings[] = array(
+            'title' => __('Pay.nl settings', 'woocommerce-payment-paynl'),
+            'type' => 'title',
+            'desc' => '<p>' . $loadedPaymentMethods . '</p><p>' . __('The following options are required to use the Pay.nl Payment Gateway and are used by all Pay.nl Payment Methods', 'woocommerce-payment-paynl') . '</p>',
+            'id' => 'paynl_global_settings',
+        );
+        $addedSettings[] = array(
+            'name' => __('Api token', 'woocommerce-payment-paynl'),
+            'type' => 'text',
+            'desc' => __('The api token used to communicate with the Pay.nl API, you can find your token <a href="https://admin.pay.nl/my_merchant" target="api_token">here</a>', 'woocommerce-payment-paynl'),
+            'id' => 'paynl_apitoken',
+        );
+        $addedSettings[] = array(
+            'name' => __('Service id', 'woocommerce-payment-paynl'),
+            'type' => 'text',
+            'desc' => __('The serviceid to identify your website, you can find your serviceid <a href="https://admin.pay.nl/programs/programs" target="serviceid">here</a>', 'woocommerce-payment-paynl'),
+            'id' => 'paynl_serviceid',
+            'desc_tip' => __('The serviceid should be in the following format: SL-xxxx-xxxx', 'woocommerce-payment-paynl'),
+        );
+        $addedSettings[] = array(
+            'name' => __('Send order data', 'woocommerce-payment-paynl'),
+            'type' => 'checkbox',
+            'desc' => __('Check this box if you want to send the order data to pay.nl, this is required if you want to use afterpay', 'woocommerce-payment-paynl'),
+            'id' => 'paynl_send_order_data',
+            'default' => 'yes',
+        );
+        $addedSettings[] = array(
+            'name' => __('Payment screen language', 'woocommerce-payment-paynl'),
+            'type' => 'select',
+            'options' => array(
+                'nl' => __('Dutch', 'woocommerce-payment-paynl'),
+                'en' => __('English', 'woocommerce-payment-paynl'),
+                'de' => __('German', 'woocommerce-payment-paynl'),
+                'es' => __('Spanish', 'woocommerce-payment-paynl'),
+                'it' => __('Italian', 'woocommerce-payment-paynl'),
+                'fr' => __('French', 'woocommerce-payment-paynl'),
+            ),
+            'desc' => __('This is the language in which the paymetn screen will be shown', 'woocommerce-payment-paynl'),
+            'id' => 'paynl_language',
+            'default' => 'nl',
+        );
+        $addedSettings[] = array(
+            'name' => __('Bankselection', 'woocommerce-payment-paynl'),
+            'type' => 'select',
+            'options' => array(
+                'none' => __('No bankselection', 'woocommerce-payment-paynl'),
+                'select' => __('Selectbox', 'woocommerce-payment-paynl'),
+                'radio' => __('Radiobuttons', 'woocommerce-payment-paynl'),
+            ),
+            'desc' => __('Pick the type of bankselection', 'woocommerce-payment-paynl'),
+            'id' => 'paynl_bankselection',
+            'default' => 'none',
+        );
+
+        $addedSettings[] = array(
+            'type' => 'sectionend',
+            'id' => 'paynl_global_settings',
+        );
+        foreach ($settings as $setting)
+        {
+            if (isset($setting['id']) && $setting['id'] == 'payment_gateways_options' && $setting['type'] != 'sectionend')
+            {
+                $updatedSettings = array_merge($updatedSettings, $addedSettings);
+            }
+            $updatedSettings[] = $setting;
+        }
+
+
+        return $updatedSettings;
+    }
+
+    /**
+     * This function registers the Pay Payment Gateways
+     */
+    public static function register()
+    {
+        add_filter('woocommerce_payment_gateways', array(__CLASS__, '_getGateways'));
+    }
+
+    /**
+     * This function adds the Pay Global Settings to the woocommerce payment method settings
+     */
+    public static function addSettings()
+    {
+        add_filter('woocommerce_payment_gateways_settings', array(__CLASS__, '_addGlobalSettings'));
+    }
+
+    /**
+     * Register the API's to catch the return and exchange
+     */
+    public static function registerApi()
+    {
+        add_action('woocommerce_api_wc_pay_gateway_return', array(__CLASS__, '_onReturn'));
+        add_action('woocommerce_api_wc_pay_gateway_exchange', array(__CLASS__, '_onExchange'));
+    }
+
+    public static function _onReturn()
+    {
+        $status = self::getStatusFromStatusId($_GET['orderStatusId']);
+
+        $url = Pay_Helper_Transaction::processTransaction($_GET['orderId'], $status);
+
+        wp_redirect($url);
+    }
+
+    public static function _onExchange()
+    {
+        ob_start();
+        try
+        {   
+            $pending = false;
+            if ($_GET['action'] == 'new_ppt')
+            {
+                $status = self::STATUS_SUCCESS;
+            } elseif ($_GET['action'] == 'pending')
+            {
+                $status = self::STATUS_PENDING;
+                $pending = true;
+                $message = 'TRUE|Ignoring pending';  
+            } elseif ($_GET['action'] == 'cancel')
+            {
+                $status = self::STATUS_CANCELED;
+            }
+            if(!$pending){
+                $url = Pay_Helper_Transaction::processTransaction($_GET['order_id'], $status);
+                $message = 'TRUE|Status updated to ' . $status;
+            }
+        } catch (Pay_Exception $e)
+        {
+            $message = 'False|Error: ' . $e->getMessage();
+        }
+        $suppressed = ob_get_clean();
+        
+        if(!empty($suppressed)){
+            $message .=' |Suppressed Output: '.$suppressed;
+        }
+        die($message);
+    }
+
+    public static function getStatusFromStatusId($statusId)
+    {
+        if ($statusId == '100')
+        {
+            $status = self::STATUS_SUCCESS;
+        } elseif ($statusId < 0)
+        {
+            $status = self::STATUS_CANCELED;
+        } else
+        {
+            $status = self::STATUS_PENDING;
+        }
+        return $status;
+    }
+
+}
---
+++ woocommerce-payment-paynl.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Plugin Name: Woocommerce Pay.nl Payment Methods
+ * Plugin URI: https://wordpress.org/plugins/woocommerce-paynl-payment-methods/
+ * Description: Pay.nl payment methods for woocommerce
+ * Version: 2.3.12
+ * Author: andypay
+ * Author URI: http://www.pay.nl
+ * Requires at least: 3.0.1
+ * Tested up to: 4.3.1
+ *
+ * Text Domain: woocommerce-payment-paynl
+ * Domain Path: /i18n/languages/
+ */
+//Autoloader laden en registreren
+require_once dirname(__FILE__) . '/includes/classes/Autoload.php';
+
+//plugin functies inladen
+require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+
+//textdomain inladen
+load_plugin_textdomain('woocommerce-payment-paynl', false, 'woocommerce-paynl-payment-methods/i18n/languages');
+
+function error_woocommerce_not_active() {
+    echo '<div class="error"><p>' . __('The Pay.nl payment methods plugin requires woocommerce to be active', 'woocommerce-payment-paynl') . '</p></div>';
+}
+
+function error_curl_not_installed() {
+    echo '<div class="error"><p>' . __('Curl is not installed.<br />In order to use the Pay.nl payment methods, you must install install CURL.<br />Ask your system administrator to install php_curl', 'woocommerce-payment-paynl') . '</p></div>';
+}
+
+// Curl is niet geinstalleerd. foutmelding weergeven
+if (!function_exists('curl_version')) {
+    add_action('admin_notices', 'error_curl_not_installed');
+}
+
+
+
+//Autoloader registreren
+Pay_Autoload::register();
+
+//Installer registreren
+register_activation_hook(__FILE__, array('Pay_Setup', 'install'));
+
+if(is_plugin_active_for_network('woocommerce-paynl-payment-methods/woocommerce-payment-paynl.php')) {
+    add_action('wpmu_new_blog', array('Pay_Setup', 'newBlog'), 10, 6);
+}
+
+if (is_plugin_active('woocommerce/woocommerce.php') || is_plugin_active_for_network('woocommerce/woocommerce.php')) {
+
+    //Gateways van pay aan woocommerce koppelen
+    Pay_Gateways::register();
+
+    //Globale settings van pay aan woocommerce koppelen
+    Pay_Gateways::addSettings();
+
+    //Return en exchange functies koppelen aan de woocommerce API
+    Pay_Gateways::registerApi();
+} else {
+    // Woocommerce is niet actief. foutmelding weergeven
+    add_action('admin_notices', error_woocommerce_not_active);
+}
+
